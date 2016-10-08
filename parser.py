893cee9083becc4be9fcaa5656aeb3cb1c925f1d
Qing-Cheng Li <qc.linux@gmail.com>
2016-10-09 00:15:02 +0800

    #2 handle title and description cases

--- parser.py
+++ parser.py
@@ -61,20 +61,69 @@ def parse_testcase_file(filename):
 
     testcase_file = open(filename, 'r')
 
-    parse_result = {
-                "testcase_title": filename,
-                "testcase_description": filename,
-                "sections": []
-            }
+    line_results = []
 
+    # read file line by line
     line_number = 0
     for line in testcase_file:
         line_number += 1
         line_result = tokenliz_line(line, line_number)
         if line_result:
-            print line_number, line_result
+            line_results.append(line_result)
 
+        #print line_number, line_result
     testcase_file.close()
+    #print line_results
+
+    ### Build Parsed Result ###
+
+    testcase = {    # TODO: change default values
+                "title": filename,
+                "description": "",
+                "sections": []
+            }
+
+    current_section = {
+                "title": None,
+                "description": "",
+                "actions": []
+            }
+
+    for line_result in line_results:
+        line_type = line_result["type"]
+        if line_type == "title":
+            if line_result["level"] == 1:
+                testcase["title"] = line_result["content"]
+            else:
+                # already had another section, here need to start a new one
+                if current_section["title"]:
+                    # save previous one into sections of testcase
+                    testcase["sections"].append(current_section)
+                    # create a new one then assing title to it.
+                    current_section = {
+                                "title": line_result["content"],
+                                "description": "",
+                                "actions": []
+                            }
+
+                # here appears the first section
+                else:
+                    current_section["title"] = line_result["content"]
+
+        elif line_type == "description":
+            # in a section
+            if current_section["title"]:
+                current_section["description"] += "%s\n" % (line_result["content"])
+
+            # there is no section
+            else:
+                testcase["description"] += "%s\n" % (line_result["content"])
+
+        elif line_type == "step":
+            # TODO - parse actions in step
+            pass
+
+    print testcase
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser(description='Zizhi\'s parser')
