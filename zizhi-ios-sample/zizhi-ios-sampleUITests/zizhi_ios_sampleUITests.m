f13a3f9234f290f24a4f3722ac8f6712f6a763d5
Qing-Cheng Li <qc.linux@gmail.com>
2016-10-17 00:22:04 +0800

    #3 #4 just copy generated code into test project

--- zizhi-ios-sample/zizhi-ios-sampleUITests/zizhi_ios_sampleUITests.m
+++ zizhi-ios-sample/zizhi-ios-sampleUITests/zizhi_ios_sampleUITests.m
@@ -44,6 +44,41 @@
     XCTAssertNotNil(app.staticTexts[@"Hello world"]);
 }
 
+- (void) test_Test_Case_Name {
+    // Given something
+    ZZMatcher *zzm_1 = [[ZZMatcher alloc] initWithIdentifier:@"Some where"];
+    ZZMatcher *zzm_2 = [[ZZMatcher alloc] initWithIdentifier:@"Some button" elementType:XCUIElementTypeButton];
+    zzm_2.ancestor = zzm_1;
+    XCUIElement *targetElement_1 = [Zizhi findElementByZizhiMatcher:zzm_2];
+    [targetElement_1 tap];
+    
+    ZZMatcher *zzm_3 = [[ZZMatcher alloc] initWithIdentifier:@"Another where"];
+    ZZMatcher *zzm_4 = [[ZZMatcher alloc] initWithIdentifier:@"Some label"];
+    zzm_4.ancestor = zzm_3;
+    XCUIElement *targetElement_2 = [Zizhi findElementByZizhiMatcher:zzm_4];
+    XCTAssertNotNil(targetElement_2);
+    
+    // When doing some actions
+    ZZMatcher *zzm_5 = [[ZZMatcher alloc] initWithElementType:XCUIElementTypeNavigationBar];
+    ZZMatcher *zzm_6 = [[ZZMatcher alloc] initWithIdentifier:@"A" elementType:XCUIElementTypeButton];
+    zzm_6.ancestor = zzm_5;
+    XCUIElement *targetElement_3 = [Zizhi findElementByZizhiMatcher:zzm_6];
+    [targetElement_3 tap];
+    
+    ZZMatcher *zzm_7 = [[ZZMatcher alloc] initWithIdentifier:@"C" elementType:XCUIElementTypeCollectionView];
+    ZZMatcher *zzm_8 = [[ZZMatcher alloc] initWithIdentifier:@"b" elementType:XCUIElementTypeButton];
+    zzm_8.ancestor = zzm_7;
+    XCUIElement *targetElement_4 = [Zizhi findElementByZizhiMatcher:zzm_8];
+    [targetElement_4 tap];
+    
+    // Then something should happend
+    ZZMatcher *zzm_9 = [[ZZMatcher alloc] initWithIdentifier:@"some icons" elementType:XCUIElementTypeImage];
+    XCUIElement *targetElement_5 = [Zizhi findElementByZizhiMatcher:zzm_9];
+    XCTAssertNotNil(targetElement_5);
+    
+    
+}
+
 - (void)testExampleTwo {
     // Use recording to get started writing UI tests.
     
